ee399b5a3df358e0353910aa5b18b58656ec0e71
Lord World <mr.pro100y.patsan@mail.ru>
2022-10-11 13:14:41 +0700

    Добавьте файлы проекта.

---
+++ MathModeling/Program.cs
@@ -0,0 +1,14 @@
+﻿double number = 0.2152;
+int counter = 0;
+while (counter < 100)
+{
+    Console.WriteLine(number + "   " + number * number);
+    ++counter;
+    number = Randomize(number);
+}
+
+static double Randomize(double number)
+{
+    string squareNumber = (number * number).ToString("0.00000000");
+    return double.Parse(squareNumber[0..2] + squareNumber[4..8]);
+}
---
+++ Multiplication/Program.cs
@@ -0,0 +1,9 @@
+﻿int M = 1000, a = 37, b = 1, N = 1, x = 1;
+double u;
+while (N <= 100)
+{
+    x = (a * x + b) % M;
+    u = Convert.ToDouble(x) / Convert.ToDouble(M);
+    Console.WriteLine("x{0}: {1}, u{2}: {3}", N, x, N, u);
+    ++N;
+}
